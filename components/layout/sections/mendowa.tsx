1a97ba6da97975907adeefdf24daa4b91a1ceda0
Hwayeeon <yoonakim320@gmail.com>
2024-12-27 04:09:27 +0700

    Add page.tsx and mendowa.tsx

---
+++ components/layout/sections/mendowa.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+
+export const HeroSection = () => {
+  const { theme } = useTheme();
+  return (
+    <section className="">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="text-2xl font-extrabold text-white-900">All <span className="text-red-500">Links</span></h2>
+      </div>
+      
+    </section>
+  );
+};
