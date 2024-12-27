adb0bf964f7fad29f23e7fe18b707d510c9b2310
Hwayeeon <yoonakim320@gmail.com>
2024-12-27 11:20:32 +0700

    Add new position kevintaswn

--- components/layout/sections/team.tsx
+++ components/layout/sections/team.tsx
@@ -48,7 +48,7 @@ export const TeamSection = () => {
         "https://images.unsplash.com/photo-1534528741775-53994a69daeb?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       firstName: "Kevin",
       lastName: "Taswin",
-      positions: ["UI/UX Designer"],
+      positions: ["UI/UX Designer", "DevOps"],
       socialNetworks: [
         {
           name: "LinkedIn",
