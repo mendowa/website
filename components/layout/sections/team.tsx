52ae9e00401893e9f5ed622833cd94bcc8b4139f
Hwayeeon <yoonakim320@gmail.com>
2024-12-27 11:24:10 +0700

    Add new position

--- components/layout/sections/team.tsx
+++ components/layout/sections/team.tsx
@@ -69,7 +69,7 @@ export const TeamSection = () => {
         "https://images.unsplash.com/photo-1527980965255-d3b416303d12?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       firstName: "Alek",
       lastName: "Wibu",
-      positions: ["Machine Learning Engineer", "TensorFlow Tinkerer"],
+      positions: ["Machine Learning Engineer", "UI/UX Designer"],
       socialNetworks: [
         {
           name: "LinkedIn",
